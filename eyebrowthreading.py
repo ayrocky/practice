b78c139a246fd16ecc0d7167758b68ea3a07aba7
Ashif Yousuf <ashif.yousuf@pason.com>
2014-04-16 15:10:30 -0300

    add user module for login from google web app service

--- eyebrowthreading.py
+++ eyebrowthreading.py
@@ -1,12 +1,20 @@
 """Just checking the app engine"""
 
+from google.appengine.api import users
 import webapp2
+
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Eyebrow Threading Halifax!')
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('''Eyebrow Threading Halifax!
+                                thanks for comming here ''' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 
 application = webapp2.WSGIApplication([
